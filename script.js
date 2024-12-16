37381ce2e5c4c1dac17f14fd1f03c92e55099b64
ChAl76 <chornyalexander@gmail.com>
2024-12-16 09:49:21 +0200

    add svg and tooltip

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Constants
   const dataURL =
     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
-  const margin = { top: 100, right: 50, bottom: 100, left: 100 };
-  const width = 1200 - margin.left - margin.right;
+  const margin = { top: 60, right: 100, bottom: 100, left: 100 };
+  const width = 1300 - margin.left - margin.right;
   const height = 600 - margin.top - margin.bottom;
   const monthNames = [
     'January',
@@ -20,6 +19,20 @@ document.addEventListener('DOMContentLoaded', () => {
     'December',
   ];
 
-  // Create main container elements
-  const container = d3.select('body').append('div').attr('id', 'container');
+  const svg = d3
+    .select('body')
+    .append('svg')
+    .attr('id', 'svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('class', 'graph')
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  // Tooltip
+  const tooltip = d3
+    .select('body')
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0);
 });
